750b972a33cb19dd94412360ac1594f26b151211
Nick Payne <nick@kurai.co.uk>
2011-08-19 22:09:42 +0100

    swapped roles around a bit - client now pushes data to a much simpler server

--- server.js
+++ server.js
@@ -2,48 +2,51 @@
 
 var util = require('util'),
     spawn = require('child_process').spawn,
+    querystring = require('querystring'),
     http = require('http');
 
-// was going to take path from the input request, but it makes more sense
-// for the script to take the path from argv
-var path = process.argv[2];
+var createServer = function() {
+    http.createServer(function(req, res) {
+        // simply listen out for POST requests
+        if (req.method == 'POST') {
+            var body = '';
+            req.on('data', function(data) {
+                body += data;
+            });
+            req.on('end', function() {
+                var postvars = querystring.parse(body);
 
-// right, initially, let's feed everything into gource to get it up-to-date
-var origDir = process.cwd();
-try {
-    process.chdir(path);
-} catch (e) {
-    console.log("Bad dir argument ["+path+"]");
-    process.exit(1);
+                // we assume revdata is lovely git log info ready for gource
+                // obviously, this needs more checking as at the moment it's just
+                // a totally blind proxy
+                process.stdout.write(postvars.revdata);
+
+                res.writeHead(200, {'Content-Type' : 'text/plain'});
+                res.end('OK');
+            });
+        }
+    }).listen(2424);
 }
 
-var log = spawn('git', ['log',  '--pretty=format:user:%aN%n%ct', '--reverse', '--raw', '--encoding=UTF-8', '--no-renames']);
-log.stdout.on('data', function(data) {
-    process.stdout.write(data);
-});
-
-log.on('exit', function(code) {
-    if (code) throw new Error('Bad Code: '+code);
-
-    http.createServer(function(req, res) {
-        updateStdout(path, res);
-    }).listen(2424, "127.0.0.1");
-
-    var updateStdout = function(path, res) {
-        var log = spawn('git', ['log',  '--pretty=format:user:%aN%n%ct', '--reverse', '--raw', '--encoding=UTF-8', '--no-renames', 'HEAD^..']);
-        log.stdout.on('data', function(data) {
-            process.stdout.write(data);
-        });
-
-        log.on('exit', function(code) {
-            // anything?
-            if (code == 0) {
-                res.writeHead(200, {'Content-Type' : 'application/json'});
-                res.end('{"ok":"true"}\n');
-            } else {
-                res.writeHead(500, {'Content-Type' : 'application/json'});
-                res.end('{"err":"'+code+'"}\n');
-            }
-        });
+// optional initial population for gource can come from argv
+if (typeof process.argv[2] != 'undefined') {
+    var path = process.argv[2];
+    try {
+        process.chdir(path);
+    } catch (e) {
+        console.log("Bad dir argument ["+path+"]");
+        process.exit(1);
     }
-});
+
+    var log = spawn('git', ['log',  '--pretty=format:user:%aN%n%ct', '--reverse', '--raw', '--encoding=UTF-8', '--no-renames']);
+    log.stdout.on('data', function(data) {
+        process.stdout.write(data);
+    });
+
+    log.on('exit', function(code) {
+        if (code) throw new Error('Bad Code: '+code);
+        createServer();
+    });
+} else {
+    createServer();
+}
